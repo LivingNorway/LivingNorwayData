a1dcbc0ce9d01fc5f044efb6a7913455f290828f
Matthew Grainger <matthew.grainger@nina.no>
2020-06-09 22:02:29 +0200

    Add seapop data

--- data-raw/mydataset.R
+++ data-raw/mydataset.R
@@ -10,9 +10,17 @@ Rock_ptarmigan <- read.csv("C:/Users/matthew.grainger/Documents/Projects_in_deve
 saveRDS(Rock_ptarmigan,"Rock_ptarmigan.RDS")
 Rock_ptarmigan <- readRDS("C:/Users/matthew.grainger/Documents/Projects_in_development/LivingNorwayData/data-raw/Rock_ptarmigan.RDS")
 
+#Seapop
+Seapop <- read_delim("data-raw/Seapop.csv",
+                          ";", escape_double = FALSE, trim_ws = TRUE)
+saveRDS(Seapop,"Seapop.RDS")
+Seapop <- readRDS("C:/Users/matthew.grainger/Documents/Projects_in_development/LivingNorwayData/data-raw/Seapop.RDS")
+
 # This should be the last line.
 # Note that names are unquoted.
 # I like using overwrite = T so everytime I run the script the
 # updated objects are saved, but the default is overwrite = F
 
 usethis::use_data(Rock_ptarmigan, overwrite = T)
+
+usethis::use_data(Seapop, overwrite = T)
